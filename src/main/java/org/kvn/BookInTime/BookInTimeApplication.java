f07e812774fd41d98052ba7db531fa04aefb1b0d
Raghu <k.raghu9926@gmail.com>
2025-01-13 10:45:24 +0530

    initial commit

---
+++ src/main/java/org/kvn/BookInTime/BookInTimeApplication.java
@@ -0,0 +1,13 @@
+package org.kvn.BookInTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookInTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookInTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kvn/BookInTime/BookInTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kvn.BookInTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookInTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
